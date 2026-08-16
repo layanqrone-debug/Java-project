4f0b46296ded8af32802e407874c775125439c23
layanqrone-debug <layanqr.one@gmail.com>
2026-08-16 12:27:48 +0300

    Add files via upload

--- Arr.java
+++ Arr.java
@@ -1,48 +1,46 @@
 import java.util.Scanner;
-public class Arr{
-public static void main (String[]args){
-    Scanner input = new Scanner(System.in);
-    
-    System.out.print("enter size of first array ");
-    int size1 = input.nextInt();
-    int[] values1 = new int[size1];
-    System.out.print("enter size of second array ");
-    int size2 = input.nextInt();
-    int[] values2 = new int[size2];
-    
-    System.out.print("enter "+ values1.length+" numbers ");
-    for(int i=0 ; i<values1.length ; i++ )
-     values1[i] = input.nextInt();
-     
-    System.out.print("enter "+ values2.length+" numbers ");
-    for(int i=0 ; i<values2.length ; i++ )
-    values2[i] = input.nextInt();
-    
-if(identical(values1 , values2)) {
-    System.out.print("the array are identical");
-}    
- else{
-    System.out.print("the array are not identical"); 
- }   
-}
-
-public static boolean identical(int[] numbers1 , int[] numbers2){
-    if(numbers1.length!=numbers2.length)
-    return false ;
-    for(int i =0 ; i<numbers1.length;i++)
-    if(numbers1[i]!=numbers2[i]){
-        return false;
+public class Main {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        int[][]arr=new int[3][4];
+System.out.println("write "+arr.length + "x"+ arr[0].length +"value");
+    for(int row=0 ; row<arr.length ; row++) {
+        for(int columns= 0; columns<arr[row].length; columns++){
+        arr[row][columns]  = input.nextInt();
+        }
     }
-        return true;
+    int positive=0, negative=0 , zero=0 ,sumpos=0 , sumneg=0;
+     for(int row=0 ; row<arr.length ; row++) {
+        for(int columns= 0; columns<arr[row].length; columns++){
+    if(arr[row][columns]>0){
+        positive++;
+        sumpos+=arr[row][columns];
     }
+    if (arr[row][columns]<0) {
+        negative++;
+        sumneg=arr[row][columns];
+    }
+    if  (arr[row][columns]==0){
+        zero++;
+    }
+        }
+             }
+    System.out.println("positive values "+positive +" sum positive number "+sumpos);
+    System.out.println("negative values "+negative+" sum of negative number "+sumneg);
+    System.out.println("zero values"+zero);
+    
+    int sumfirstrow=0 ;
+for(int columns=0 ;columns<arr[0].length; columns++){
+    sumfirstrow+=arr[0][columns] ;
 }
-/*
-Output :
-enter size of first array 4
-enter size of second array 3
-enter 4 numbers 1 2 3 4
-enter 3 numbers 1 4 6
-the array are not identical
-*/    
-
+    System.out.println("sum of first row"+sumfirstrow);
+    
+    int sumseconcol=0;
+    for (int row=0; row<arr.length ; row++){
+       sumseconcol+=arr[row][2]; 
+    }
+    System.out.println("sum of second columns"+sumseconcol);
+    
+        }
+    }   
 
