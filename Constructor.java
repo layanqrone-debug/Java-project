e4fed729e2f3468631f6f69f1348982a9e7d6dc0
Layan Azzam <layanqr.one@gmail.com>
2026-09-15 11:14:19 +0300

    Add a new small projrct to the repo

---
+++ Constructor.java
@@ -0,0 +1,49 @@
+public  class Constructor{
+    public static void main(String[]args){
+    Student s1 = new Student();
+    Student s2 = new Student("layan" , 20 , "MIS" , 3.8);
+    Student s3 = new Student(s2);
+    Student s4 = new Student("jana",2.6);
+    s1.display();
+    s2.display();
+    s3.display();
+    s4.display();
+}
+}
+  class Student {
+  String  name ;
+  int  age ;
+  String  major ;
+  double  gpa ;  
+//defult
+  Student()  {
+      
+  }
+    //parameterized
+  Student(String n ,int a  ,String m , double g ) {
+      this.name= n;
+      this.age= a;
+      this.major= m;
+      this.gpa= g;
+  } 
+    //copy
+   Student(Student s2) {
+     name= s2.name;
+      age= s2.age;
+     major= s2.major;
+    gpa= s2.gpa; 
+}
+   Student(String n , double g ){
+ this.name= n;
+ this.gpa= g;     
+   }
+ public void display(){
+     System.out.println("name = "+name); 
+     
+     System.out.println("age = "+ age);
+     
+     System.out.println("major = "+major);
+     
+     System.out.println("gpa = "+gpa);
+ }   
+}
