2de713666777605a6dfecfd2fdc98aa4e77c4112
Layan Azzam <layanqr.one@gmail.com>
2026-09-20 11:27:07 +0300

    Enhance code readability with comments
    Added comments to clarify input handling and string operations.

--- Functions.java
+++ Functions.java
@@ -6,26 +6,37 @@
    int Applenum=0 , totalnum=0 ;
     while(true){
 System.out.print("enter a laptop type or /finish/ to end ");
-
+     
+      //hasNextInt() : to detect if the user typed a number so we can show an error massage before processing 
      if (input.hasNextInt()) {
 System.out.println("ERROR! numbers are not allowed.");
 input.next();
    continue; 
 }
+     //nextLine() : reads an entire line of text including spaces 
+     //trim() : removes any accidental spaces from the beginning and the end 
+     //toLowerCase() : converts all letters in the string to lowercase to make it insensitive for 
 type = input.nextLine().trim().toLowerCase();
-        
+     
+      //equals() : compares two string to check if they are identical   
     if(type.equals("finish")){
     break;
 }
+     
+     //isEmpty() : to prevent the program from processing if the user just presses 'Enter' 
         if (type.isEmpty()){
             continue;
-        }
+        } 
+     
+     //startsWith() : checks if the string begins with specific prefix 
      if(type.startsWith("apple")) {
          Applenum++;
      }
         totalnum++;
     }
      System.out.printf("\nApple laptops: %d\nTotal laptops: %d\n ", Applenum , totalnum);
+
+  //close() : closes the 'Scanner' object prevent memory leaks
      input.close();
  }
  }
